5cd42f6457f24f7880efc95b09b5020394083091
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2019-05-03 11:46:22 +0100

    Added markdownify filter; getting netlify cms going

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,24 @@
 module.exports = function (eleventyConfig) {
 
+    const md = require('markdown-it')({
+        html: false,
+        breaks: true,
+        linkify: true
+        // typographer: true
+    });
+
+    eleventyConfig.addNunjucksFilter("markdownify", markdownString => md.render(markdownString));
+
     // Don't process folders with static assets e.g. images
     eleventyConfig.addPassthroughCopy("admin");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("_headers");
 
+
+
     return {
         passthroughFileCopy: true,
+        dataTemplateEngine: "njk",
         dir: {
             input: ".",
             includes: "_includes",
