11c228c82e832a5ee48cfd4f073688cb794f3acd
Ed Johnson-Williams <ed@johnsonwilliams.co.uk>
2019-05-04 23:28:42 +0100

    making next singings be calculated on build

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,21 +3,6 @@ import './collapse.js';
 
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-	const sched = later.parse.recur().on(7).dayOfWeek().on(2, 4)
-		.dayOfWeekCount();
-	const nextThree = later.schedule(sched).next(3);
-	const options = {
-		month: "long",
-		day: "numeric",
-	};
-	const firstNext = (nextThree[0].toLocaleDateString("en-GB", options));
-	const secondNext = (nextThree[1].toLocaleDateString("en-GB", options));
-	const thirdNext = (nextThree[2].toLocaleDateString("en-GB", options));
-	const upcomingsingings = `<p>Our next singings are on: <span class="font-italic font-weight-bold"><span id="next-singing">Saturday ${firstNext}</span><span class="font-weight-normal">, </span><span id="second-next-singing"> ${secondNext}</span><span class="font-weight-normal">, and </span><span id="third-next-singing"> ${thirdNext}</span><span class="font-weight-normal">.</span></span></p>`;
-	document.getElementById("upcoming-singings").innerHTML = upcomingsingings;
-});
-
 // Hides menu when clicked
 document.addEventListener("DOMContentLoaded", () => {
 	const nav_items = document.getElementsByClassName("nav-item");
