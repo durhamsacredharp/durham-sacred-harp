be26699d7a1b2441b500c8963c6731077eb106fe
Ed <ed@DA-MUS-10.lan>
2020-03-04 21:20:47 +0000

    making js work on IE

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,38 @@
-import './util.js';
-import './collapse.js';
-
 "use strict";
 
 // Hides menu when clicked
-document.addEventListener("DOMContentLoaded", () => {
-	const nav_items = document.getElementsByClassName("nav-item");
-	const navbarButton = document.getElementsByClassName("navbar-toggler")[0];
-	const navbarToggler = document.getElementById("navbarToggler");
-	const main = document.getElementById("main");
 
-	Array.from(nav_items).forEach((nav_item) => {
-		nav_item.addEventListener("click", () => {
-			if (navbarToggler.classList.contains("show")) {
-				navbarButton.click();
-			}
-		});
-	});
+document.addEventListener("DOMContentLoaded", function() {
+  var nav_items = Array.from(document.getElementsByClassName("nav-item"));
+  var navbarButton = document.getElementsByClassName("navbar-toggler")[0];
+  var navbarToggler = document.getElementById("navbarToggler");
+  var main = document.getElementById("main");
+  nav_items.forEach(function(nav_item) {
+    nav_item.addEventListener("click", function() {
+      if (navbarToggler.classList.contains("show")) {
+        navbarButton.click();
+      }
+    });
+  });
 
-	// Hides the menu if anywhere other than the menu is clicked
-	main.addEventListener("click", () => {
-		if (navbarToggler.classList.contains("show")) {
-			navbarButton.click();
-		}
-	});
+  // Hides the menu if anywhere other than the menu is clicked
+
+  main.addEventListener("click", function() {
+    if (navbarToggler.classList.contains("show")) {
+      navbarButton.click();
+    }
+  });
 });
 
 // On page load, if there's a hash in the URL then scroll up to offset the sticky header
-document.addEventListener("DOMContentLoaded", () => {
-	if (window.location.hash) {
-		const navbarHeight = document.getElementById("navbar-row").offsetHeight + 10;
-		scrollBy(0, -navbarHeight);
-	}
+document.addEventListener("DOMContentLoaded", function() {
+  if (window.location.hash) {
+    var navbarHeight = document.getElementById("navbar-row").offsetHeight + 10;
+    scrollBy(0, -navbarHeight);
+  }
 });
-
 // Scroll up if a new hash is clicked to offset the sticky header
-window.addEventListener("hashchange", () => {
-	const navbarHeight = document.getElementById("navbar-row").offsetHeight + 10;
-	scrollBy(0, -navbarHeight);
+window.addEventListener("hashchange", function() {
+  var navbarHeight = document.getElementById("navbar-row").offsetHeight + 10;
+  scrollBy(0, -navbarHeight);
 });
